ad5d96a25f4626f02ee3b32fdf4b2e9a28e4a741
Lucas Martin Alvarez Bisordi <lukas_alvarez@hotmail.com>
2025-10-29 08:26:16 -0300

    first commit

---
+++ src/config/views.py
@@ -0,0 +1,22 @@
+import base64
+
+from django.conf import settings
+from django.views.generic import TemplateView
+
+
+class ViewerV1TemplateView(TemplateView):
+    template_name = 'viewer_v1.html'
+
+
+class ViewerV2TemplateView(TemplateView):
+    template_name = 'viewer_v2.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        dicom_path = settings.BASE_DIR.parent / 'inputs' / 'dicom.dcm'
+
+        with open(dicom_path, 'rb') as file:
+            ctx['dicom_base64'] = base64.b64encode(file.read()).decode('utf-8')
+
+        return ctx
